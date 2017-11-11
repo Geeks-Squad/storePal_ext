554cd0642b9155387d62c4e8bd2411d052e50370
Chaitanya <prndyala@gmail.com>
2017-11-11 10:38:34 +0530

    Send URL automatically to the server

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 chrome.webRequest.onBeforeSendHeaders.addListener(function(details){
     chrome.tabs.getSelected(null,function(tab) {
         document.getElementById("url").value  = tab.url;
-        document.getElementById("tabid").value = tab.id;
     });
     document.getElementById("time").value = Date();
     document.getElementById("submit").click();
